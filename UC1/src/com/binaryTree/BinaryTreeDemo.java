9fad23a1b67453494687e0437792e04429f72e32
tomthomas437@gmail.com <tomthomas437@gmail.com>
2021-12-09 18:53:45 +0530

    Create a Binary Search Tree

---
+++ UC1/src/com/binaryTree/BinaryTreeDemo.java
@@ -0,0 +1,27 @@
+package com.binaryTree;
+import java.util.Scanner;
+
+public class BinaryTreeDemo {
+	
+	public static void main(String args[]) {
+	            
+	        Scanner scan = new Scanner(System.in);
+	        /* Creating object of BT */
+	        BinaryTreeServices bt = new BinaryTreeServices(); 
+	            bt.insert(56);
+	            bt.insert(30);
+	            bt.insert(70);
+	            
+	            System.out.print("\n Binary Search Tree ");
+	            System.out.print("\nPost order : ");
+	            bt.postorder();
+	            System.out.print("\nPre order : ");
+	            bt.preorder();
+	            System.out.print("\nIn order : ");
+	            bt.inorder();
+	            
+	}
+}
+	            
+	           
+  
